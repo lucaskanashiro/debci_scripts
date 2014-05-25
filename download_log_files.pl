1754f346c094e3f057ce00d5bc7d64237fd51057
Lucas Kanashiro <lucas-kanashiro@hotmail.com>
2014-05-25 14:55:47 +0000

    Updating to download *autopkg.log files

--- download_log_files.pl
+++ download_log_files.pl
@@ -6,6 +6,7 @@ use Encode;
 use WWW::Mechanize;
 
 my $url = 'http://ci.debian.net/data/unstable-amd64/packages/';
+
 my $finder = WWW::Mechanize->new();
 
 my $totalFails=0;
@@ -49,7 +50,28 @@ foreach (@prefix){
 		my $url_package = $url_prefix . $_;
 
 		$finder->get($url_package);
+
+		my @package_html = split("\n", $finder->content);
+		my @package_file;
+
+		foreach (@package_html){
+			if($_ =~ m/<a href="([^"]+)"/){
+				push(@package_file, $1);
+			}
+		}
 		
+		my @reversed_package_file = reverse(@package_file);
+		my $name_autopkg_log="";
+
+		foreach (@reversed_package_file){
+			if($_ =~ m/autopkgtest.log/){
+				$name_autopkg_log = $_;
+				last;
+			}
+		}
+
+		my $url_autopkg_log_file = $url_package . $name_autopkg_log;
+
 		if($finder->content =~ m/latest.log/){
 			my $url_log = $url_package . "latest.log";
 
@@ -62,15 +84,25 @@ foreach (@prefix){
 					$_ =~ s/%2b/+/;
 					my $filename = $_ . "-fail.log";
 					$finder->save_content($filename, binmode => ':raw', decoded_by_headers => 1);
+
+					$finder->get($url_autopkg_log_file);
+					$filename = $_ . "-autopkg-fail.log";
+					$finder->save_content($filename, binmode => ':raw', decoded_by_headers => 1);
+
 				}elsif($1 eq "tmpfail "){
 					$contTmpFail = $contTmpFail+1;
 					$_ =~ s/\///;
 					$_ =~ s/%2b/+/;
 					my $filename = $_ . "-tmpfail.log";
 					$finder->save_content($filename, binmode => ':raw', decoded_by_headers => 1);
+
+					$finder->get($url_autopkg_log_file);
+					$filename = $_ . "-autopkg-fail.log";
+					$finder->save_content($filename, binmode => ':raw', decoded_by_headers => 1);
 				}
 			}
 		}
+
 	}
 
 	$totalFails = $totalFails + $contFail;
@@ -79,3 +111,4 @@ foreach (@prefix){
 
 print "\n\nFAIL: " . $totalFails . "\n";
 print "TMPFAIL: " . $totalTmpFails . "\n";
+
